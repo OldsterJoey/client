80a3d5d6a143d00ea03987fb1afb3a6c750d940a
iri6ka <ikonnova@gmail.com>
2021-07-30 13:12:01 +1000

    sets up SignIn page, authServices and global state with state Context

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import Home from './pages/index'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import SignIn from './components/SignIn/SignIn';
 
+import React,{useState, useReducer, useEffect} from 'react';
+import {StateContext} from './utils/stateContext'
+
+
+
 
 function App() {
+
+  const initialState = {
+		wishes: [],
+		loggedInUser: null,
+		auth: {token: null}
+	}
+	const [store, dispatch] = useReducer(stateReducer,initialState)
   return (
-    <Router>
-      <Switch>
-        <Route path="/signin" component={SignIn} exact/>
-        <Route path="/" component={Home} exact/>
-      </Switch>
-    </Router>
+    <div>
+      <StateContext.Provider value={{store,dispatch}}>
+        <Router>
+          <Switch>
+            <Route path="/signin" component={SignIn} exact/>
+            <Route path="/" component={Home} exact/>
+          </Switch>
+        </Router>
+      </StateContext.Provider>
+    </div>
   )
 }
 
