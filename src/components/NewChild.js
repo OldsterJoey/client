301b0606a0bfae2049675cc6b19925ea29398478
OldsterJoey <jjcsull@gmail.com>
2021-08-08 14:35:08 +1000

    Successfully connected to rails API

--- src/components/NewChild.js
+++ src/components/NewChild.js
@@ -5,75 +5,75 @@ import {createChild, getChild, updateChild} from '../services/childrenServices'
 import{userPics} from '../utils/userPics'
 import {useGlobalState} from '../utils/stateContext'
 
-export default function NewChild() {
-	const initialFormState = {
-        id: "",
-		name:  "",
-        icon: "",
-	}
-	const [formState,setFormState] = useState(initialFormState)
-	let history = useHistory()
-	let {id} = useParams()
-	const {dispatch, store} = useGlobalState()
-	const {children} = store;
-    console.log()
+export default function NewChild() {}
+// 	const initialFormState = {
+//         id: "",
+// 		name:  "",
+//         icon: "",
+// 	}
+// 	const [formState,setFormState] = useState(initialFormState)
+// 	let history = useHistory()
+// 	let {id} = useParams()
+// 	const {dispatch, store} = useGlobalState()
+// 	const {children} = store;
+//     console.log()
 
-	useEffect(() => {
-		if(id) {
-			getChild(id)
-			.then((child) => {
-				console.log(child)
-				setFormState({
-					child_id: child.id,
-					name: child.name, 
-                    icon: child.img_id
-				})
-			})
-		}
-	},[id])
+// 	useEffect(() => {
+// 		if(id) {
+// 			getChild(id)
+// 			.then((child) => {
+// 				console.log(child)
+// 				setFormState({
+// 					child_id: child.id,
+// 					name: child.name, 
+//                     icon: child.img_id
+// 				})
+// 			})
+// 		}
+// 	},[id])
 
-	function getLastId() {
-		console.log(children)
-		const ids = children.map(child => child.id)
-		console.log(ids)
-		return Math.max(...ids)
-	}
+// 	function getLastId() {
+// 		console.log(children)
+// 		const ids = children.map(child => child.id)
+// 		console.log(ids)
+// 		return Math.max(...ids)
+// 	}
 
-	function handleChange(event) {
-		setFormState({
-			...formState,
-			[event.target.name]: event.target.value
-		})
+// 	function handleChange(event) {
+// 		setFormState({
+// 			...formState,
+// 			[event.target.name]: event.target.value
+// 		})
 		
-	}
-	function handleClick(event) {
-		event.preventDefault()
-		if(id) {
-			updateChild( {id: id, ...formState})
-			.then(() => {
-				dispatch({type: 'updateChild', data: {id: id, ...formState}})
-				history.push(`/children/${id}`)
-			})
-		}
-		else {
-			const nextId = getLastId() + 1;
-			createChild({...formState, id: nextId})
-			.then((child) => {
-				dispatch({type: 'createChild', data: child})
-				history.push('/main')
-			})
-			.catch((error) => console.log(error))
-		}
-	}
-	return (
-		<div>
-			<Label>User Picture:</Label>
-			<select name='userPic_id' value={formState.userPic.src} onChange={handleChange}>
-				{userPics.map((userPic) => <option key={userPic.id} value={userPic.id}>{userPic.src}</option>)}
-			</select>
-			<Label>Child:</Label>
-			<BigTextInput type='text' name='name' value={formState.name} onChange={handleChange}></BigTextInput>
-			<Button onClick={handleClick}>{id ? 'Update' : 'Create'}</Button>
-		</div>
-	)
-}
+// 	}
+// 	function handleClick(event) {
+// 		event.preventDefault()
+// 		if(id) {
+// 			updateChild( {id: id, ...formState})
+// 			.then(() => {
+// 				dispatch({type: 'updateChild', data: {id: id, ...formState}})
+// 				history.push(`/children/${id}`)
+// 			})
+// 		}
+// 		else {
+// 			const nextId = getLastId() + 1;
+// 			createChild({...formState, id: nextId})
+// 			.then((child) => {
+// 				dispatch({type: 'createChild', data: child})
+// 				history.push('/main')
+// 			})
+// 			.catch((error) => console.log(error))
+// 		}
+// 	}
+// 	return (
+// 		<div>
+// 			<Label>User Picture:</Label>
+// 			<select name='userPic_id' value={formState.userPic.src} onChange={handleChange}>
+// 				{userPics.map((userPic) => <option key={userPic.id} value={userPic.id}>{userPic.src}</option>)}
+// 			</select>
+// 			<Label>Child:</Label>
+// 			<BigTextInput type='text' name='name' value={formState.name} onChange={handleChange}></BigTextInput>
+// 			<Button onClick={handleClick}>{id ? 'Update' : 'Create'}</Button>
+// 		</div>
+// 	)
+// }
--- src/services/childrenServices.js
+++ src/services/childrenServices.js
@@ -1,63 +1,36 @@
 import {wishlists} from './wishlistsServices.js'
-import{userPics} from '../utils/userPics'
-export const children = [
-        {
-          "id": 1,
-          "name": "Joey",
-          "created_at": "2021-08-07T04:28:12.187Z",
-          "updated_at": "2021-08-07T04:28:12.187Z",
-          "user_id": 1,
-          "wish_list": {
-            "name": "joeyswishlist",
-            "wishes": [
-              {
-                "name": "I wish for a pink bicycle"
-              },
-              {
-                "name": "I wish for a nintendo"
-              }
-            ]
-          }
-        },
-        {
-          "id": 2,
-          "name": "Irina",
-          "created_at": "2021-08-07T04:28:12.193Z",
-          "updated_at": "2021-08-07T04:28:12.193Z",
-          "user_id": 2
-          
-        }
-      ]
+import server from '../config/api';
 
 // helper methpod to descructure data of children
 function transformChildren(child){
     const wishlist = wishlists.find(wishlist => wishlist.id === child.id)
-    const icon = userPics.find(icon=> icon.id === child.img_id )
     return{
         id: child.id,
         name: child.name,
         wishlist: wishlist.name,
-        icon: icon.src
     }
 
 }
 
 export async function getChildren(){
-    return children
+    const response = await server.get('/api/child_profiles');
+    console.log(response);
+    return response.data
 }
 
 export async function getChild(id){
-    const child = children.find(child => child.id === id)
-    return child ? transformChildren(child) : null
+    const response = await server.get('/api/child_profiles/${id}');
+    console.log(response);
+    return response.data
 }
 
-export async function createChild(child){
-    return child
-}
-export async function deleteChild(id){
-    console.log('child has been removed')
-    return id
-}
-export async function updateChild(child){
-    return child
-}
+// export async function createChild(child){
+//     return child
+// }
+// export async function deleteChild(id){
+//     console.log('child has been removed')
+//     return id
+// }
+// export async function updateChild(child){
+//     return child
+// }
