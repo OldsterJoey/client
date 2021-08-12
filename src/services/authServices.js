e22e4bf2e464fef72b5e24441d18e90ad3716145
IrinaKonnova <irina.konnova@plutora.com>
2021-08-12 17:23:42 +1000

    prepares application for deployment, sets up firebase

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,13 +1,13 @@
 import server from '../config/api'
 
 export async function signUp(data) {
-	const response = server.post('/api/auth/sign_up', data)
+	const response = await server.post('/api/auth/sign_up', data)
 	console.log(response)
 	return response.data
 }
 
 export async function signIn(data) {
-	const response = server.post('/api/auth/sign_in', data)
+	const response = await server.post('/api/auth/sign_in', data)
 	console.log(response)
 	return response.data
 }
